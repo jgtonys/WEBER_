b3dbd70a3ab657aaa9ef3ee18ffb254de276639c
Jungyu Kim <33674947+jgtonys@users.noreply.github.com>
2019-06-19 11:45:59 +0900

    Update index.js

--- src/script/index.js
+++ src/script/index.js
@@ -1,3 +1,7 @@
+/*
+메인 스크립트 인덱싱 파일
+*/
+
 document.write("<script src='src/script/handler.js'></script>");
 document.write("<script src='src/script/form.js'></script>");
 document.write("<script src='src/script/operator.js'></script>");
